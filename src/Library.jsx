977c931479fafb2bedefa20e7bf88e574faac3e4
Karan-Bharti1 <karan.codes1@gmail.com>
2025-02-05 10:42:31 +0530

    feat: added functionality to provide data in input fields , add the book and display it on the view

--- src/Library.jsx
+++ src/Library.jsx
@@ -1,12 +1,37 @@
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
+import { addBook, calculateTotal } from "./actions"
+
 const Library=()=>{
+    const [book,setBook]=useState({
+        title:"",
+        author:"",
+        ISBN:""
+    })
+    const dispatch=useDispatch()
+    const books=useSelector(state=>state.books)
+    const total=useSelector(state=>state.total)
+    const handleChange=(key,value)=>{
+setBook({...book,[key]:value})
+    }
+const addBookHandler=()=>{
+    if(book.title && book.author && book.ISBN){
+        dispatch(addBook(book))
+        setBook({title:"",author:"",ISBN:""})
+        dispatch(calculateTotal())
+    }
+}
+const displayBooks=books.map(book=><li key={book.ISBN}>{book.title} by {book.author}(ISBN:{book.ISBN})</li>)
 return(
     <>
     <h1>Library Management</h1>
-    <input type="text" placeholder="Title"/>
-    <input type="text" placeholder="Author" />
-    <input type="text" placeholder="ISBN"/>
-    <button>Add Book</button>
+    <input type="text" onChange={(event)=>handleChange("title",event.target.value)} value={book.title} placeholder="Title"/>
+    <input type="text" onChange={(event)=>handleChange("author",event.target.value)} value={book.author}  placeholder="Author" />
+    <input type="text" onChange={(event)=>handleChange("ISBN",event.target.value)} value={book.ISBN} placeholder="ISBN"/>
+    <button onClick={addBookHandler}>Add Book</button>
     <h2>Library Summary</h2>
+    <p>Total Books:{total}</p>
+{displayBooks}
     </>
 )
 }
--- src/actions.js
+++ src/actions.js
@@ -1,3 +1,5 @@
 export const ADD_BOOKS="books/added"
 export const REMOVE_BOOKS="books/remove"
 export const CALCULATE_TOTAL="books/calculateTotal"
+export const addBook=(book)=>({type:ADD_BOOKS,payload:book})
+export const calculateTotal=()=>({type:CALCULATE_TOTAL})
