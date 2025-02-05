a47c5a83f0f647dfd3d9aea565baa1152620d2ec
Karan-Bharti1 <karan.codes1@gmail.com>
2025-02-05 09:48:51 +0530

    chore: added JSX structure fot Library Component

---
+++ src/Library.jsx
@@ -0,0 +1,13 @@
+const Library=()=>{
+return(
+    <>
+    <h1>Library Management</h1>
+    <input type="text" placeholder="Title"/>
+    <input type="text" placeholder="Author" />
+    <input type="text" placeholder="ISBN"/>
+    <button>Add Book</button>
+    <h2>Library Summary</h2>
+    </>
+)
+}
+export default Library
